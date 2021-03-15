6b469fb9ae03d032fcf3782ea637178971a1d5e9
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-03-15 17:35:24 -0300

    feat: Added new function to API

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,9 @@
-import { _getDecks } from "./_DATA";
+import { _addCardToDeck, _getDecks } from "./_DATA";
 
 export function getAllDecks() {
 	return _getDecks();
+}
+
+export function addCard(deck, question, answer) {
+	return _addCardToDeck(deck, question, answer);
 }
